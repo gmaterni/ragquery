3c4a21c988cd0ed3226fbd5e52d0a165e27d4df4
giuseppe materni <giuseppe.materni@gmail.com>
2025-01-02 15:28:01 +0100

    02-01-25 (15:28)

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -196,10 +196,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const VERS = "0.1.02 (01-01-2025)";
+const VERS = "0.1.03 (02-01-2025)";
 
-// var xlog = console.log;
-var xlog=function(){};
+var xlog = console.log;
+// var xlog=function(){};
 
 const cancelRequest = () => {
   const ok = confirm("Confermi Cancellazione Richeista ?");
