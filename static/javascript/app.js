37a1e0136663d5d56b48dd99561db4ea0c82f2f5
giuseppe materni <giuseppe.materni@gmail.com>
2025-05-30 21:18:03 +0200

    30-05-25 (21:18)

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -196,7 +196,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const VERS = "0.1.16";
+const VERS = "0.1.17";
 
 // var xlog = console.log;
 var xlog = function () {};
